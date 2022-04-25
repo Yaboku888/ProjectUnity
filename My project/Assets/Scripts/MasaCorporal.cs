9e05e1fddd6c59c1e50acce6db737f1e6bdc3bb9
yato <yatochris1904@gmail.com>
2022-04-25 15:12:06 -0500

    IndiceMasaCorporal.md

---
+++ My project/Assets/Scripts/MasaCorporal.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasaCorporal : MonoBehaviour
+{
+    public string NombreDeUsuario;
+    public float AlturaEnCentimetros;
+    public float PesoEnKilogramo;
+    private float multiplicacion;
+    private float division;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(NombreDeUsuario);
+        Debug.Log(AlturaEnCentimetros);
+        Debug.Log(PesoEnKilogramo);
+        multiplicacion = (AlturaEnCentimetros * AlturaEnCentimetros);
+        division = (PesoEnKilogramo / multiplicacion);
+        Debug.Log(division);
+        
+        if (division <18.5)
+        {
+            Debug.Log(" Usuario " + NombreDeUsuario + " Tas bien Seco ");
+        }
+        if (division >=18.5)
+        {
+            Debug.Log("MASA NORMAL PAPU");
+
+        }
+        if (division >=25-0)
+        {
+            Debug.Log("estas algo pasado de masa");
+
+        }
+        if (division >=29.9)
+        {
+            Debug.Log("Tas bien gordo");
+        }
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
